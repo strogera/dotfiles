5c6c5c9b54bd5bf250bc5f6cfd3751b18a3b2161
strogera <26325577+strogera@users.noreply.github.com>
2021-04-22 22:49:07 +0300

    Dwm autostart patch

--- st/config.def.h
+++ st/config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=16:antialias=true:autohint=true";
+static char *font = "JetBrains Mono:pixelsize=16:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
 	"LiberationMono Nerd Font Mono:pixelsize=16:antialias=true:autohint=true", 
